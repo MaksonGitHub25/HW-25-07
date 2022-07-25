6a16ff73dc91e6b4ae6ebc13a1a6576a2588736d
MaksonGitHub25 <makson94021@gmail.com>
2022-07-25 19:38:19 +0300

    complete first task

--- app.js
+++ app.js
@@ -23,85 +23,108 @@ const arr = [
     }
 ];
 
-let almostFiltredArray = arr.filter(function (elem) {
-    return typeof elem == 'object';
-});
+const textFields = document.querySelectorAll('.text_space');
 
-let filtredArray = almostFiltredArray.filter(function (elem) {
-    return !_.isNull(elem);
-});
+let filtredArray;
+function filterArr(mainArr) {
+    let almostFiltredArray = mainArr.filter(function (elem) {
+        return typeof elem == 'object';
+    });
+    
+    filtredArray = almostFiltredArray.filter(function (elem) {
+        return !_.isNull(elem);
+    });
+}
 
-let currentObjectInObject = filtredArray.filter(function (elem) {
-    if (!_.isArray(elem)) {
-        currentObj = _.get(elem, 'currentObject');
-        return currentObj;
-    }
-});
+filterArr(arr);
 
-currentObjectInObject.filter(function (elem) {
-    for (let key in elem) {
-        let value = elem[key];
-        arrayWithcurrentObjectValue = [];
-        if (Array.isArray(value)) {
-            arrayWithcurrentObjectValue = [...arrayWithcurrentObjectValue, value];
+let sumAllCurrentObjNumber;
+function checkCurrentObject(filterArr) {
+    let currentObjectInObject = filterArr.filter(function (elem) {
+        if (!_.isArray(elem)) {
+            currentObj = _.get(elem, 'currentObject');
+            return currentObj;
         }
-    }
-    return arrayWithcurrentObjectValue;
-});
-
-arrayWithcurrentObjectValue.forEach(function (elem) {
-    elem.forEach(function (item) {
-        arrayWithcurrentObjectValue = [...arrayWithcurrentObjectValue, item];
     });
-});
-arrayWithcurrentObjectValue.shift();
-// выровнял негра к одному уровню
-
-let currentObjValue = [];
-arrayWithcurrentObjectValue.forEach(function (elem) {
-    elem.forEach(function (item) {
-        currentObjValue = [...currentObjValue, item];
+    
+    currentObjectInObject.filter(function (elem) {
+        for (let key in elem) {
+            let value = elem[key];
+            arrayWithcurrentObjectValue = [];
+            if (Array.isArray(value)) {
+                arrayWithcurrentObjectValue = [...arrayWithcurrentObjectValue, value];
+            }
+        }
+        return arrayWithcurrentObjectValue;
     });
-});
-
-let currentObjNumbers = currentObjValue.map(function (elem) {
-    if (_.isNaN(+elem)) {
-        helloMessage = elem;
-    } else {
-        elem = parseInt(elem);
-    }
-    return elem;
-});
-_.pull(currentObjNumbers, helloMessage);
-
-const initialValue = 0;
-let sumAllCurrentObjNumber = currentObjNumbers.reduce(function (previousValue, currentValue) {
-    return previousValue + currentValue;
-}, initialValue);
-
-let arrayWithFields = filtredArray.filter(function (elem) {
-    if (_.isArray(elem)) {
+    
+    arrayWithcurrentObjectValue.forEach(function (elem) {
+        elem.forEach(function (item) {
+            arrayWithcurrentObjectValue = [...arrayWithcurrentObjectValue, item];
+        });
+    });
+    arrayWithcurrentObjectValue.shift();
+    // выровнял негра к одному уровню
+    
+    let currentObjValue = [];
+    arrayWithcurrentObjectValue.forEach(function (elem) {
+        elem.forEach(function (item) {
+            currentObjValue = [...currentObjValue, item];
+        });
+    });
+    
+    let currentObjNumbers = currentObjValue.map(function (elem) {
+        if (_.isNaN(+elem)) {
+            helloMessage = elem;
+        } else {
+            elem = parseInt(elem);
+        }
         return elem;
-    }
-});
-
-arrayWithFields.forEach(function (elem) {
-    elem.forEach(function (item) {
-        arrayWithFields = [...arrayWithFields, item];
     });
-});
-arrayWithFields.shift();
+    _.pull(currentObjNumbers, helloMessage);
+    
+    const initialValue = 0;
+    sumAllCurrentObjNumber = currentObjNumbers.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue;
+    }, initialValue);
+}
 
-let fields = arrayWithFields.filter(function (elem) {
-    if (_.isObject(elem)) {
-        return elem;
-    }
-});
+checkCurrentObject(filtredArray);
+
+let fields;
+function getFieldsFromArr(filterArr) {
+    let arrayWithFields = filtredArray.filter(function (elem) {
+        if (_.isArray(elem)) {
+            return elem;
+        }
+    });
+    
+    arrayWithFields.forEach(function (elem) {
+        elem.forEach(function (item) {
+            arrayWithFields = [...arrayWithFields, item];
+        });
+    });
+    arrayWithFields.shift();
+    
+    fields = arrayWithFields.filter(function (elem) {
+        if (_.isObject(elem)) {
+            return elem;
+        }
+    });
+
+    return fields;
+}
+
+getFieldsFromArr(filtredArray);
 
 let requiredValue;
+let addedNumber = [];
 function checkObjOnRequired(obj) {
     Object.keys(obj).forEach(function (key) {
         let value = obj[key];
+        if (_.isNumber(value)) {
+            addedNumber = [...addedNumber, value];
+        }
         if (key === 'required') {
             requiredValue = value;
         } else {
@@ -111,26 +134,98 @@ function checkObjOnRequired(obj) {
     return requiredValue;
 }
 
-console.log('almostFiltredArray', almostFiltredArray);
-console.log('filtredArray', filtredArray);
-console.log('currentObjectInObject', currentObjectInObject);
-console.log('arrayWithcurrentObjectValue', arrayWithcurrentObjectValue);
-console.log('currentObjValue', currentObjValue);
-console.log('task 1.2', helloMessage);
-console.log('currentObjNumbers', currentObjNumbers);
-console.log('task 1.3', sumAllCurrentObjNumber);
-console.log('arrayWithFields', arrayWithFields);
-console.log('fields', fields);
-console.log('task 1.1', 'requiredValue', checkObjOnRequired(fields));
+function checkArrayOnNumber(arr) {
+    arrayWithNumberInArr = [];
+    arr.forEach(function (elem) {
+        if (_.isNumber(elem)) {
+            arrayWithNumberInArr = [...arrayWithNumberInArr, elem];
+        } else if (_.isArray(elem)) {
+            arrayWithNumberInArr = [...arrayWithNumberInArr, checkArrayOnNumber(elem)];
+        } else if (_.isObject(elem)) {
+            arrayWithNumberInArr = [...arrayWithNumberInArr, checkObjOnNumber(elem)];
+        }
+    });
+    return arrayWithNumberInArr;
+}
 
+function checkObjOnNumber(obj) {
+    arrayWithNumberInObj = [];
+    Object.keys(obj).forEach(function (key) {
+        let value = obj[key];
+        if (_.isNumber(value)) {
+            arrayWithNumberInObj = [...arrayWithNumberInObj, value];
+        } else if (_.isArray(value)) {
+            arrayWithNumberInObj = [...arrayWithNumberInObj, checkArrayOnNumber(value)];
+        } else if (_.isObject(value)) {
+            arrayWithNumberInObj = [...arrayWithNumberInObj, checkObjOnNumber(value)];
+        }
+    });
+    return arrayWithNumberInObj;
+}
 
-//* 1.1) Вывести в консоль свойство required .
-//* 1.2) Получить “Hello world” из currentObject .
-//* 1.3) Сложить все значения которые можно в currentObject
-// 1.4) Написать функцию которая будет 23 из fields добавлять ко всем числам что лежат в массиве arr .
+let allNumberObjArray = [];
+arr.forEach(function (elem) {
+    if (_.isNumber(elem)) {
+        allNumberObjArray = [...allNumberObjArray, elem];
+    } else if (_.isArray(elem)) {
+        allNumberObjArray = [...allNumberObjArray, checkArrayOnNumber(elem)];
+    } else if (_.isObject(elem)) {
+        allNumberObjArray = [...allNumberObjArray, checkObjOnNumber(elem)];
+    }
+});
+
+function deleteUsedArray(arr) {
+    arr.forEach(function (elem, index) {
+        if (_.isArray(elem)) {
+            arr.splice(index, 1);
+        }
+    });
+}
+
+function copyNumberInMainArray(arr) {
+    arr.forEach(function (elem) {
+        if (_.isArray(elem)) {
+            elem.forEach(function (item) {
+                allNumberObjArray = [...allNumberObjArray, item];
+                if (_.isArray(item)) {
+                    copyNumberInMainArray(item);
+                }
+            });
+        }
+    });
+    deleteUsedArray(allNumberObjArray);
+}
+
+copyNumberInMainArray(allNumberObjArray);
+
+function addNumberOfFieldToNumberOfArr(arrWithMainNumber, arrWithAddNumber) {
+    let arrayWithAddedNumberArray = {};
+    arrWithAddNumber.forEach(function (elem) {
+        const numberForAdd = elem;
+        
+        const addNumberArray = arrWithMainNumber.map((num) => num + numberForAdd);
+        addNumberArray.sort(function (a, b) {
+            return a-b;
+        });
+        arrayWithAddedNumberArray[numberForAdd] = addNumberArray;
+    });
+    return arrayWithAddedNumberArray;
+}
+
+function printAllTOLog() {
+    console.log('filtredArray', filtredArray);
+    console.log('task 1.1', 'requiredValue', checkObjOnRequired(fields));
+    console.log('task 1.2', helloMessage);
+    console.log('task 1.3', sumAllCurrentObjNumber);
+    console.log('task 1.4');
+    console.log('DefaultNumberArray', allNumberObjArray);
+    console.log('AddedNumberArray', addNumberOfFieldToNumberOfArr(allNumberObjArray, addedNumber));
+}
+
+printAllTOLog();
 
 // 2)
-// const arrOfNums = [ [23 , 32 ,15 , 18] , [13 ,31 ,23 ,32 ] , [“Hello world”] , 23 ]
+// const arrOfNums = [ [23 , 32 ,15 , 18] , [13 ,31 ,23 ,32 ] , [“Hello world”] , 23 ];
 
 // 2.1) Вырезать arrOfNums первый(0) и второй(1) элемент из массива и сформировать из них новы массив , например : kek
 // 2.2) В массиве kek -> не должно быть одинаковых чисел , можно создать новый массив .
